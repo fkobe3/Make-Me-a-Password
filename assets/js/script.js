56d132c85e27de646e11fa0cb4349afa78cc7b3f
Frank Kobe <fkobe3@gmail.com>
2022-06-23 07:57:09 -0400

    README added, as well as prompts added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,28 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+function getPasswordLength() {
 
-  passwordText.value = password;
+var passwordLength = prompt("Please type in the length of your password (must be from 8 to 128 characters) .");
+  while (passwordLength < 8 || passwordLength > 128 || passwordLength == null) {
+     alert("Your answer did not meet the minimum, or exceeded the maximum, limits. Please try again.")
+     passwordLength = prompt("Please type in the length of your password(must be from 8 to 128 characters).");
+  }
+  return passwordLength;};
 
+function initializePassword() {
+   
+  alert("What does your password need to include?");
+ 
+  var Lowercase = confirm("Lowercase letters?");
+  var Uppercase = confirm("Uppercase letters?");
+  var Numerics = confirm("Numbers?");
+  var Specials = confirm("Special characters?");
+
+  var confirmArray = new Array(Lowercase, Uppercase, Numerics, Specials);
+  
+  var allPromptsFalseCheck = confirmArray.every( confirmElement  => !confirmElement );
+  if (allPromptsFalseCheck) {
+     alert("Please Select Character Types")
+     initializePassword();   
+  };
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
