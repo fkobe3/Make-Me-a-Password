fd6f10aff8ee0b7119e24c46792d8b25978f5971
Frank Kobe <fkobe3@gmail.com>
2022-06-23 14:06:10 -0400

    Forgot to commit the rest of the code, finished project

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,4 +25,49 @@ function initializePassword() {
      alert("Please Select Character Types")
      initializePassword();   
   };
+
+  const lowercase = 'abcdefghijklmnopqrstuvwxyz';
+  const UPPERCASE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const numbers = '1234567890';
+  const specials = `!"#$%&'()*+,-./:;<>=?@[]^_\{}|~`;
+
+  var characterRange = '';
+
+  if (Lowercase) { 
+     characterRange += lowercase; 
+  }
+  if (Uppercase) { 
+     characterRange += UPPERCASE; 
+  }
+  if (Numerics) { 
+     characterRange += numbers; 
+  }
+  if (Specials) { 
+     characterRange += specials; 
+  }
+ 
+  return characterRange;
+};
+
+function generatePassword() {
+
+ 
+  var userLength = getPasswordLength();
+  var characters = initializePassword();
+  
+  var yourPassword = '';  
+  for (let i = 0; i < userLength; i++) {
+     yourPassword += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return yourPassword;
+};
+
+var generateBtn = document.querySelector("#generate");
+
+function writePassword() {
+ var password = generatePassword();
+ var passwordText = document.querySelector("#password");
+
+ passwordText.value = password;
 }
+generateBtn.addEventListener("click", writePassword);
